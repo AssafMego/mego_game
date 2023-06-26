044fbfd33fb24ae9b16e51418d5b783f17935bdb
AssafMego <137800024+AssafMego@users.noreply.github.com>
2023-06-26 17:03:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+
+def determine_num_of_buyers(num_of_popsicles, num_of_customers, price):
+    num_of_buyers = 0
+    for customer in range(num_of_customers):
+        willing_to_pay = random.uniform(1,3)
+        if willing_to_pay > price:
+            if num_of_buyers < num_of_popsicles:
+                num_of_buyers += 1
+    return num_of_buyers
+
+
+def simulate_day(num_of_popsicles, price, num_of_customers = 100):
+    num_of_buyers = determine_num_of_buyers(num_of_popsicles, num_of_customers, price)
+    print(f"Customers today: {num_of_buyers}")
+    income = num_of_buyers * price               # Income for the day
+    return income
+
+    return income
+
+
+def main():
+    buying_price = 1
+    while True:
+        print(f"the price of each popsicles is: {buying_price}")
+        print("Enter how many popsicles you want to buy (or 'q' to quit):")
+        num_of_popsicles_input = input("> ")
+
+        if num_of_popsicles_input.lower() == "q":
+            break
+
+        num_of_popsicles = int(num_of_popsicles_input)
+
+        print("Enter the price in which you want to sell each popsicle:")
+        price_input = input("> ")
+
+        price = float(price_input)
+        expense = buying_price * num_of_popsicles
+        income = simulate_day(num_of_popsicles, price)
+        print(f"Income for the day: ${income - expense}\n")
+
+    # print(f"\nTotal income: ${total_income}")
+    print("Game Over!")
+
+if __name__ == "__main__":
+    main()
